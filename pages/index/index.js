67db36118da7a8bcd466c4a25944d03910a53bf0
Wang-xiaozhang <529203645@qq.com>
2019-12-05 15:01:35 +0800

    修改index页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,7 @@
 // js
 Page({
   data: {   
-      option1: [
-        { text: "全部商品", value: 0 },
-        { text: "新款商品", value: 1 },
-        { text: "活动商品", value: 2 }
-      ],
-      value1: 0,
+    imageURL: "/images/tabbar/code.png",
   
     timer: null, // 保存定时器
     scrollTop: 5, // 设定触发条件的距离
